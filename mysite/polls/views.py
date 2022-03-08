80e54df70cd577663dfb02e714514dcbdd89595e
floatingnahid22 <nahidhossain>
2022-03-08 13:49:21 +0600

    update views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,13 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+def detail(request, question_id=34):
+    return HttpResponse("You're looking at question.")
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
